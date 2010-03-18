69dddf9df4e45c5090c65857173f5197bdffb77e
Bernd Stramm <bernd.stramm@gmail.com>
2010-03-18 15:11:23 -0400

    bump version number in case I want to release any of this

--- src/version.cpp
+++ src/version.cpp
@@ -17,7 +17,7 @@
 
 namespace deliberate {
 
-  std::string ProgramVersion("0.3.0");
+  std::string ProgramVersion("0.3.1");
 
   std::string ProgramName("drss");
   std::string copyright ("Copyright (C) 2009,2010 Bernd H Stramm");
